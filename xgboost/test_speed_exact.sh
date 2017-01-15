b951a239ad74dd610e9492464c09bad05e52ab93
Laurae <Laurae2@users.noreply.github.com>
2017-01-15 02:52:52 +0100

    Add files via upload

--- xgboost/test_speed_exact.sh
+++ xgboost/test_speed_exact.sh
@@ -1,3 +1,3 @@
-C:/Compiled/xgboost/xgboost xgboost_12.conf tree_method=exact data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_12.log
-C:/Compiled/xgboost/xgboost xgboost_06.conf tree_method=exact data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_06.log
-C:/Compiled/xgboost/xgboost xgboost_01.conf tree_method=exact data="../data/higgs.train" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_01.log
+C:/Compiled/xgboost/xgboost xgboost_12.conf tree_method=exact data="../data/higgs.train_xgb" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_12.log
+C:/Compiled/xgboost/xgboost xgboost_06.conf tree_method=exact data="../data/higgs.train_xgb" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_06.log
+C:/Compiled/xgboost/xgboost xgboost_01.conf tree_method=exact data="../data/higgs.train_xgb" objective="binary:logistic" 2>&1 | tee xgboost_higgs_speed_01.log
